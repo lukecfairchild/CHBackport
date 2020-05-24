ba4e4d05ec8473640e273ad9408f08fe7ec9a1e3
Luke Fairchild <lukecfairchild@gmail.com>
2020-05-23 22:56:16 -0600

    typo, indenting

--- src/main/java/com/kookster/chbackport/Functions.java
+++ src/main/java/com/kookster/chbackport/Functions.java
@@ -24,48 +24,48 @@ import java.util.Arrays;
 
 public class Functions {
 
-    @api(environments = CommandHelperEnvironment.class)
-    public static class executeas extends AbstractFunction {
+	@api(environments = CommandHelperEnvironment.class)
+	public static class executeas extends AbstractFunction {
 
-        public String getName() {
-            return "executeas";
-        }
+		public String getName() {
+			return "executeas";
+		}
 
-        public Integer[] numArgs() {
-            return new Integer[]{Integer.MAX_VALUE};
-        }
+		public Integer[] numArgs() {
+			return new Integer[]{Integer.MAX_VALUE};
+		}
 
-        public String docs() {
-            return "mixed {player, label, [values...,] closure} Executes the given closure in the context of a given"
-                    + " player. A closure that runs player(), for instance, would return the specified player's name."
-                    + " The label argument sets the permission label that this closure will use. If null is given,"
-                    + " the current label will be used, like with execute().";
-        }
+		public String docs() {
+			return "mixed {player, label, [values...,] closure} Executes the given closure in the context of a given"
+					+ " player. A closure that runs player(), for instance, would return the specified player's name."
+					+ " The label argument sets the permission label that this closure will use. If null is given,"
+					+ " the current label will be used, like with execute().";
+		}
 
-        public Class<? extends CREThrowable>[] thrown() {
-            return new Class[]{CRECastException.class};
-        }
+		public Class<? extends CREThrowable>[] thrown() {
+			return new Class[]{CRECastException.class};
+		}
 
-        public Boolean runAsync() {
-            return null;
-        }
+		public Boolean runAsync() {
+			return null;
+		}
 
-        public boolean isRestricted() {
-            return true;
-        }
+		public boolean isRestricted() {
+			return true;
+		}
 
-        @Override
-        public Construct exec(Target t, Environment environment, Construct... args) throws ConfigRuntimeException {
-            if(!(args[args.length - 1] instanceof CClosure)) {
-                throw new CRECastException("Only a closure (created from the closure function) can be sent to executeas()", t);
-            }
-            Construct[] vals = new Construct[args.length - 3];
-            System.arraycopy(args, 2, vals, 0, args.length - 3);
-            CClosure closure = (CClosure) args[args.length - 1];
-            CommandHelperEnvironment cEnv = closure.getEnv().getEnv(CommandHelperEnvironment.class);
-            GlobalEnv gEnv = closure.getEnv().getEnv(GlobalEnv.class);
+		@Override
+		public Construct exec(Target t, Environment environment, Construct... args) throws ConfigRuntimeException {
+			if(!(args[args.length - 1] instanceof CClosure)) {
+				throw new CRECastException("Only a closure (created from the closure function) can be sent to executeas()", t);
+			}
+			Construct[] vals = new Construct[args.length - 3];
+			System.arraycopy(args, 2, vals, 0, args.length - 3);
+			CClosure closure = (CClosure) args[args.length - 1];
+			CommandHelperEnvironment cEnv = closure.getEnv().getEnv(CommandHelperEnvironment.class);
+			GlobalEnv gEnv = closure.getEnv().getEnv(GlobalEnv.class);
 
-            MCCommandSender originalSender = cEnv.GetCommandSender();
+			MCCommandSender originalSender = cEnv.GetCommandSender();
 			MCCommandSender sender;
 
 			if(args[0].val().equals(Static.getConsoleName())) {
@@ -75,27 +75,27 @@ public class Functions {
 			} else {
 				sender = Static.GetPlayer(args[0].val(), t);
 			}
-            cEnv.SetCommandSender(sender, t));
+			cEnv.SetCommandSender(sender, t);
 
-            String originalLabel = gEnv.GetLabel();
-            if(!(args[1] instanceof CNull)) {
-                gEnv.SetLabel(args[1].val());
-            }
+			String originalLabel = gEnv.GetLabel();
+			if(!(args[1] instanceof CNull)) {
+				gEnv.SetLabel(args[1].val());
+			}
 
-            try {
-                closure.execute(vals);
-            } catch (FunctionReturnException e) {
-                return e.getReturn();
-            } finally {
-                cEnv.SetCommandSender(originalSender);
-                gEnv.SetLabel(originalLabel);
-            }
-            return CVoid.VOID;
-        }
+			try {
+				closure.execute(vals);
+			} catch (FunctionReturnException e) {
+				return e.getReturn();
+			} finally {
+				cEnv.SetCommandSender(originalSender);
+				gEnv.SetLabel(originalLabel);
+			}
+			return CVoid.VOID;
+		}
 
-        @Override
-        public CHVersion since() {
-            return CHVersion.V3_3_2;
-        }
-    }
+		@Override
+		public CHVersion since() {
+			return CHVersion.V3_3_2;
+		}
+	}
 }
